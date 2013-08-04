f3e533f2e33469fc917cac290ae7b41d459345aa
William Barrett Simms <wbsimms@gmail.com>
2013-08-04 09:06:25 -0400

    Use timer rather than blocking sleep

--- SignalRDemo/SignalRDemo/Hubs/Chat.cs
+++ SignalRDemo/SignalRDemo/Hubs/Chat.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Timers;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Timer = System.Timers.Timer;
 
 namespace SignalRDemo.Hubs
 {
     public class Chat : Hub
     {
+        static string messageToSend = DateTime.Now.ToString();
+        Timer t = new Timer(500);
+
         public void SayHello()
         {
-            while (true)
-            {
-                Clients.All.addMessage("Date time  : "+DateTime.Now);
-                Thread.Sleep(500);
-            }
+            t.Elapsed +=t_Elapsed;
+            t.Start();
+        }
+
+        private void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Clients.All.addMessage("Date time  : " + messageToSend);
+            messageToSend = DateTime.Now.ToString();
         }
     }
 }
